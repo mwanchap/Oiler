7174321de45c72b763ea16040e856881d0937102
Matt Wanchap <matt.wanchap@cpal.com.au>
2017-11-24 13:51:42 +1000

    Updated problem 1

--- problem1.py
+++ problem1.py
@@ -1,7 +1,8 @@
 def getSumOfMultiples(baseNum, count):
     the_sum = baseNum
+    max_value = int(count/baseNum) + 1
 
-    for i in range(2, count/baseNum):
+    for i in range(2, max_value):
         the_sum += baseNum * i
 
     return the_sum
@@ -11,5 +12,11 @@ def sumDivisibleBy(number, count):
     return int(number * (the_max * (the_max + 1)) /2)
 
 count = 999
+
+# attempt 1
+count2 = getSumOfMultiples(3, count) + getSumOfMultiples(5, count) - getSumOfMultiples(15, count)
+print(count2)
+
+# attempt 2
 count3 = sumDivisibleBy(3, count) + sumDivisibleBy(5, count) - sumDivisibleBy(15, count)
 print(count3)
